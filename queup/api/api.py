46fcf7b2a4fccea0114e82d5bac02613a178f2c5
Celine Liu <mangolzj@gmail.com>
2024-02-25 20:02:09 -0500

    server the react static files in flask app

--- queup/api/api.py
+++ queup/api/api.py
@@ -1,12 +1,26 @@
-from routes.user_view import user_view
-from database import app, initialize_database
+from flask import Flask, send_from_directory
+import os
 
-# Register blueprints
-app.register_blueprint(user_view)
+app = Flask(__name__)
 
-# Initialize the database
-initialize_database()
+# Define a route to serve the index.html file
+@app.route('/')
+def serve_index():
+    return send_from_directory(os.path.join('..', 'build'), 'index.html')
+
+# Define a route to serve files from the asset directory
+@app.route('/assets/<path:path>')
+def serve_asset(path):
+    return send_from_directory(os.path.join('..', 'build', 'assets'), path)
+
+# serve js files
+@app.route('/static/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory(os.path.join('..', 'build', 'static', 'js'), filename)
+# serve css files
+@app.route('/static/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory(os.path.join('..', 'build', 'static', 'css'), filename)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
-    
